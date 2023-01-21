2f63cfd5c21bf81d74dd0e824cdaab79387555ad
Cesar Segundo <cesarsegundolorenzo@gmail.com>
2023-01-20 23:11:22 -0600

    user admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,28 +23,35 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'cliente']);
 
         //Creación de usuarios
-        $userAdmin = \App\Models\User::factory()->create([
-            'name' => 'Cesar',
-            'email' => 'cesar@gmail.com'
+        // $userAdmin = \App\Models\User::factory()->create([
+        //     'name' => 'Cesar',
+        //     'email' => 'cesar@gmail.com'
+        // ]);
+        $contraseña = "12345";
+        $userAdmin = new User([
+            "email" => "cesar@gmail.com",
+            "password" => Hash::make($contraseña),
+            "name" => "Cesar",
         ]);
-        $userPersonal = \App\Models\User::factory()->create([
-            'name' => 'Jose',
-            'email' => 'jose@gmail.com'
-        ]);
-        $userPersonal2 = \App\Models\User::factory()->create([
-            'name' => 'Juan',
-            'email' => 'Juan@gmail.com'
-        ]);
-        $users = \App\Models\User::factory(5)->create();
+        $userAdmin->saveOrFail();
+        // $userPersonal = \App\Models\User::factory()->create([
+        //     'name' => 'Jose',
+        //     'email' => 'jose@gmail.com'
+        // ]);
+        // $userPersonal2 = \App\Models\User::factory()->create([
+        //     'name' => 'Juan',
+        //     'email' => 'Juan@gmail.com'
+        // ]);
+        // $users = \App\Models\User::factory(5)->create();
 
         //Asignación de roles a cada usuario
         $userAdmin->assignRole('admin');
-        $userPersonal->assignRole('personal');
-        $userPersonal2->assignRole('personal');
+        // $userPersonal->assignRole('personal');
+        // $userPersonal2->assignRole('personal');
 
-        foreach ($users as $user) {
-            $user->assignRole('cliente');
-        }
+        // foreach ($users as $user) {
+        //     $user->assignRole('cliente');
+        // }
 
         $this->call(OpeningHoursSeeder::class);
         $this->call(ServicesSeeder::class);
